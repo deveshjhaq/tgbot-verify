d8b4dbec74b45019179074788e8392cb9a3eca73
deveshjhaq <deveshjhaq@gmail.com>
2026-01-06 10:58:25 +0530

    feat: add MilitaryVerifier and update military module for SheerID verification

--- military/__init__.py
+++ military/__init__.py
@@ -1,2 +1,8 @@
-"""ChatGPT 军人 SheerID 认证模块"""
+"""ChatGPT Military SheerID Verification Module"""
+
+from .sheerid_verifier import SheerIDVerifier
+from .name_generator import NameGenerator, generate_email, generate_birth_date, generate_discharge_date
+from . import config
+
+__all__ = ['SheerIDVerifier', 'NameGenerator', 'generate_email', 'generate_birth_date', 'generate_discharge_date', 'config']
 
